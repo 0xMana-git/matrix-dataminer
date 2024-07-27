07ce99c2a069b4bb9614bd4e8959c2365356d7e2
Mana <57663038+Manaball123@users.noreply.github.com>
2024-07-27 18:28:43 +0800

    idk probably performs better now?

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,4 @@
 mkdir bin
-g++ relation-grapher/*.cpp -o bin/relation-grapher.o;
-g++ browser/*.cpp -o bin/browser.o;
+g++ -O3 relation-grapher/*.cpp -o bin/relation-grapher.o;
+g++ -O3 browser/*.cpp -o bin/browser.o;
 #g++ dataparser/*.cpp -o bin/dataparser.o;
